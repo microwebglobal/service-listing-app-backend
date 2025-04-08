414effa336ecfd04e36074a269a6b8441d7a5396
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-04-08 11:05:35 +0530

    Added longitude, latitude fields into address model

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,58 +1,70 @@
 const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    const Address = sequelize.define('Address', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'u_id'
+    const Address = sequelize.define(
+      "Address",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
         },
-        field: 'userId' 
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "users",
+            key: "u_id",
+          },
+          field: "userId",
+        },
+        type: {
+          type: DataTypes.ENUM("home", "work", "other"),
+          defaultValue: "home",
+        },
+        line1: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        line2: DataTypes.STRING,
+        city: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        state: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        postal_code: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        latitude: {
+          type: DataTypes.DECIMAL(10, 8),
+          allowNull: false,
+        },
+        longitude: {
+          type: DataTypes.DECIMAL(11, 8),
+          allowNull: false,
+        },
+        is_primary: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          field: "createdAt",
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          field: "updatedAt",
+        },
       },
-      type: {
-        type: DataTypes.ENUM('home', 'work', 'other'),
-        defaultValue: 'home'
-      },
-      line1: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      line2: DataTypes.STRING,
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      postal_code: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      is_primary: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        field: 'createdAt'
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        field: 'updatedAt'
+      {
+        tableName: "addresses",
+        timestamps: true,
       }
-    }, {
-      tableName: 'addresses',
-      timestamps: true
-    });
+    );
   
     Address.associate = function(models) {
       Address.belongsTo(models.User, {
