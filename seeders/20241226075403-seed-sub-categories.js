7e0f22692b5dedf1877daa12ffe0021c0272fe14
iamsahan <weerakkodyse@gmail.com>
2025-04-11 16:44:32 +0530

    modify subcat seeder to updte icon urls

--- seeders/20241226075403-seed-sub-categories.js
+++ seeders/20241226075403-seed-sub-categories.js
@@ -1,32 +1,36 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('sub_categories', [
-      {
-        sub_category_id: 'SCAT001',
-        category_id: 'CAT001',
-        name: 'Waxing',
-        slug: 'waxing',
-        icon_url: 'ac-repair-icon.png',
-        display_order: 1,
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        sub_category_id: 'SCAT002',
-        category_id: 'CAT002',
-        name: 'AC Services',
-        slug: 'ac-services',
-        icon_url: 'ac-repair-icon.png',
-        display_order: 1,
-        created_at: new Date(),
-        updated_at: new Date()
-      }
-    ], {});
+    await queryInterface.bulkInsert(
+      "sub_categories",
+      [
+        {
+          sub_category_id: "SCAT001",
+          category_id: "CAT001",
+          name: "Waxing",
+          slug: "waxing",
+          icon_url: "/uploads/images/1735813069408.jpg",
+          display_order: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          sub_category_id: "SCAT002",
+          category_id: "CAT002",
+          name: "AC Services",
+          slug: "ac-services",
+          icon_url: "/uploads/images/1735813069408.jpg",
+          display_order: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('sub_categories', null, {});
-  }
+    await queryInterface.bulkDelete("sub_categories", null, {});
+  },
 };
