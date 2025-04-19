804f58c77c60b9217a08f62a1c04f91e4ae30e2d
iamsahan <weerakkodyse@gmail.com>
2025-04-19 16:40:00 +0530

    add new filed to store user location to adress table

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,65 +1,73 @@
 const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    const Address = sequelize.define('Address', {
+  const Address = sequelize.define(
+    "Address",
+    {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
       userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-          model: 'users',
-          key: 'u_id'
+          model: "users",
+          key: "u_id",
         },
-        field: 'userId' 
+        field: "userId",
       },
       type: {
-        type: DataTypes.ENUM('home', 'work', 'other'),
-        defaultValue: 'home'
+        type: DataTypes.ENUM("home", "work", "other"),
+        defaultValue: "home",
       },
       line1: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       line2: DataTypes.STRING,
       city: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       state: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       postal_code: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+      },
+      map_location: {
+        type: DataTypes.GEOMETRY("POINT"),
+        allowNull: false,
       },
       is_primary: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
       },
       createdAt: {
         type: DataTypes.DATE,
-        field: 'createdAt'
+        field: "createdAt",
       },
       updatedAt: {
         type: DataTypes.DATE,
-        field: 'updatedAt'
-      }
-    }, {
-      tableName: 'addresses',
-      timestamps: true
+        field: "updatedAt",
+      },
+    },
+    {
+      tableName: "addresses",
+      timestamps: true,
+    }
+  );
+
+  Address.associate = function (models) {
+    Address.belongsTo(models.User, {
+      foreignKey: "userId",
+      targetKey: "u_id",
     });
-  
-    Address.associate = function(models) {
-      Address.belongsTo(models.User, {
-        foreignKey: 'userId',
-        targetKey: 'u_id'
-      });
-    };
-  
-    return Address;
+  };
+
+  return Address;
 };
